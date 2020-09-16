2d6e8d6d1d57aaeebd975a7cb68ddde5e5e7d531
Aman Shukla <amanshukla@Amans-MacBook-Pro.local>
2020-09-16 07:22:55 +0530

    Setup JS folder

---
+++ js/index.js
@@ -0,0 +1,12 @@
+new TypeIt(".profile-intro", {
+    strings: ["Hi, I am ","Aman Shukla","A Web Developer."],
+    speed: 100,
+    waitUntilVisible: true,
+    lifeLike:true,
+    cursor:false,
+    loop:false
+  }).go();
+
+
+
+  
